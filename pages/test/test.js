2247752777236ccae2c3be29460d051ef60de8da
xushubo <tmac523@163.com>
2018-04-03 22:08:07 +0800

    updated 2018-4-3

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,9 +1,21 @@
 var app = getApp();
+var common = require("../../common/common");
 
 Page({
   data:{
     name: "xushubo",
-    pass: null
+    pass: null,
+    id: 0,
+    condition: true
+  },
+  show: function () {
+    //var condition = this.data.condition;
+    this.setData({
+      condition: !this.data.condition
+    })
+  },
+  say: function () {
+    common.sayHello(this.data.name);
   },
   onLoad:function(options){
     this.setData({
